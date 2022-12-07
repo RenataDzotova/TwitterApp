03053ff71479e50b2be609dfb9401a9470b375c7
RenataDzotova <rdzotova@my.bcit.ca>
2022-12-06 17:45:23 -0800

    Signup and Posts

---
+++ server/project_repository.js
@@ -0,0 +1,34 @@
+const sqlite3 = require('sqlite3').verbose()
+const Promise = require('bluebird')
+
+class AppDAO {
+    // omitting other methods
+
+    get(sql, params = []) {
+        return new Promise((resolve, reject) => {
+            this.db.get(sql, params, (err, result) => {
+                if (err) {
+                    console.log('Error running sql: ' + sql)
+                    console.log(err)
+                    reject(err)
+                } else {
+                    resolve(result)
+                }
+            })
+        })
+    }
+
+    all(sql, params = []) {
+        return new Promise((resolve, reject) => {
+            this.db.all(sql, params, (err, rows) => {
+                if (err) {
+                    console.log('Error running sql: ' + sql)
+                    console.log(err)
+                    reject(err)
+                } else {
+                    resolve(rows)
+                }
+            })
+        })
+    }
+}
